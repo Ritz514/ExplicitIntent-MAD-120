ce82e2b3090c2d6cd24958fbaadbbd0b71a69bed
Ritz514 <142646304+Ritz514@users.noreply.github.com>
2024-03-14 09:29:18 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/explicitintent/MainActivity.java
+++ app/src/main/java/com/example/explicitintent/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.explicitintent;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.widget.Button;
+        import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edt1;
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edt1 = findViewById(R.id.edt1);
+        btn1 = findViewById(R.id.btn1);
+        Intent i = new Intent(MainActivity.this,MainActivity2.class);
+        btn1.setOnClickListener(View->{
+            i.putExtra("number",edt1.getText().toString());
+            startActivity(i);
+        });
+
     }
 }
